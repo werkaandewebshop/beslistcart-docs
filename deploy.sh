30382c60bf83addcf712779ee28ada5d001ea78e
Mark Wienk <mark@wienkit.nl>
2016-07-11 21:05:21 +0200

    Updated documentation

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-find ./public/* | grep -v ".git" | xargs rm -rf
-cp -R static/* public
-cd public
-git add -A
-git status
-git commit -m 'Updated documentation'
-git push origin gh-pages
